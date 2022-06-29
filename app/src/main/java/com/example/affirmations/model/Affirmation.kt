6f8865fba96fa73e698db76426b567a024676c72
Ahmed-Montasser-AbdulWahab <1700205@eng.asu.edu.eg>
2022-06-29 18:51:02 +0200

    Affirmation App Project

---
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.example.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+class Affirmation (
+    @StringRes var stringResourceId : Int,
+    @DrawableRes var imageResourceId : Int)    {
+}
